b5f9cd57e3ea32475935be47fbabbb797d5105f8
sheygs <macbookpro@sheygs-macbook.local>
2024-07-05 11:53:57 +0100

    fix(test): Jest bcrypt module

--- src/server.ts
+++ src/server.ts
@@ -7,26 +7,6 @@ const {
   app: { environment, port },
 } = config;
 
-// const server = createServer(app);
-
-// process
-//   .on('SIGINT', () => exitLog(null, 'SIGINT'))
-//   .on('SIGQUIT', () => exitLog(null, 'SIGQUIT'))
-//   .on('SIGTERM', () => exitLog(null, 'SIGTERM'))
-//   .on('uncaughtException', (error) => exitLog(error, 'uncaughtException'))
-//   .on('beforeExit', () => exitLog(null, 'beforeExit'))
-//   .on('exit', () => exitLog(null, 'exit'));
-
-// server.listen({ port }, (): void => {
-//   process.stdout.write(`⚙️ Env: ${environment}\n`);
-//   process.stdout.write(`⏱ Started on: ${Date.now()}\n`);
-//   process.stdout.write(
-//     `🚀 demo-credit api running on http://${hostname()}:${port}\n`,
-//   );
-// });
-
-// export { server };
-
 export const startServer = (app: express.Application): Server => {
   const httpServer = createServer(app);
 
